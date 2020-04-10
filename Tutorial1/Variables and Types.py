b4fe0bb50f3ff8027ca448b293aefe76fe428776
mfaefendi <25222681+mfaefendi@users.noreply.github.com>
2020-04-10 14:28:07 +0800

    First commit

---
+++ Tutorial1/Variables and Types.py
@@ -0,0 +1,15 @@
+#To define an integer, use the following syntax:
+myint = 7
+print(myint)
+
+#To define a floating point number, you may use one of the following notations:
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+#Strings are defined either with a single quote or a double quotes.
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
